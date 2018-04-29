ae8804b098ea65b1ff2717afe079060c08755d88
prashantbasnet94 <prashant.basnet@selu.edu>
2018-04-28 22:15:27 -0500

    adding files

--- colorGame.js
+++ colorGame.js
@@ -1,13 +1,13 @@
 var zero;
 var first;
 var third;
-var anyTimeConditionAssigner;
+var initialTimeConditionAssigner,afterTimeConditionAssigner;
 var h1, h2;
 var gameButton, anyButton;
-var color;
+var colorRandom, gameColor;
 
-anyTimeConditionAssigner = Math.floor(Math.random() * 6);
-
+initialTimeConditionAssigner = Math.floor(Math.random() * 6);
+afterTimeConditionAssigner = initialTimeConditionAssigner;
 function colorDeclare() {
           zero = Math.floor(Math.random() * 256);
           first = Math.floor(Math.random() * 256);
@@ -21,34 +21,105 @@ function colorAssigner() {
           var div;
           h1 = document.querySelector("h1");
           h2 = document.querySelector("h2");
-
+          while(initialTimeConditionAssigner===afterTimeConditionAssigner){
+                    initialTimeConditionAssigner = Math.floor(Math.random() * 6);
+          }
 
           for (var i = 0; i < 6; i++) {
-                    color = colorDeclare();
+                    colorRandom = colorDeclare();
 
                     anyButton = document.getElementsByTagName("button")[i];
-                    anyButton.style.background = "rgb(" + color + ")";
-                    if (i === anyTimeConditionAssigner) {
-                              h1.textContent = "rgb(" + color + ")";
-                              gameButton = document.getElementsByTagName("button")[i];
+                    anyButton.style.background = "rgb(" + colorRandom + ")";
+
+                    if (i === initialTimeConditionAssigner) {
+                              gameColor = colorRandom;
+
+                              gameButton = i;
+
                               console.log(gameButton);
-                              console.log("i:"+i);
                     }
 
 
+
           }
-
-
-          gameButton.addEventListener("click", colorAssigner());
-          gameButton.addEventListener("click", function() {
-                    h1.style.background = "rgb(" + color + ")";
-                    anyTimeConditionAssigner = Math.floor(Math.random() * 6);
-          });
-          // gameButton.addEventListener("click", function() {
-          //           alert("");
-          // });
+          //console.log(initialTimeConditionAssigner);
 }
 colorAssigner();
+
+
+//
+// if (gameButton.clicked == true) {
+//
+// }
+//
+
+//
+//
+// gameButton.addEventListener("click",function(){});
+//
+//
+
+
+
+
+
+var buttons = document.getElementsByTagName("button");
+var button;
 for (var i = 0; i < 6; i++) {
-          //anyButton[i].addEventListener("click",function(){alert("clicked")});
+          buttons[i].onclick = function() {
+                    button = this.value;
+
+                    clicked(this.value)
+          };
 }
+
+function clicked(value) {
+
+          if (value == gameButton) {
+
+                    for (var i = 0; i < 6; i++) {
+                              buttons[i].style.visibility = "visible";
+                    }
+                    initialTimeConditionAssigner = Math.floor(Math.random() * 6);
+                    h1.style.background = "rgb(" + gameColor + ")";
+                    colorAssigner();
+
+
+          } else {
+                    buttons[value].style.visibility = "hidden";
+          }
+
+}
+
+
+
+
+// initialTimeConditionAssigner = Math.floor(Math.random() * 6);
+// while(initialTimeConditionAssigner===afterTimeConditionAssigner){
+//           initialTimeConditionAssigner = Math.floor(Math.random() * 6);
+//
+//           }
+// console.log($(this).val());
+// gameButton=null;
+//
+// h1.style.background = "rgb(" + gameColor + ")";
+// colorAssigner();
+
+
+//
+// gameButton.addEventListener("click",clicked);
+//
+//
+
+
+
+
+
+
+
+
+
+//           anyTimeConditionAssigner = Math.floor(Math.random() * 6);
+//           h1.style.background = "rgb(" + color + ")";
+//           colorAssigner();
+// });
